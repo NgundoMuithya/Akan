d29e2b4c475ba33397718c038869b0c7509794f0
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-20 17:12:47 +0300

    Create a prompt for birth year.clear

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,5 +2,6 @@ var months = ["placeholder", "january", "february", "march", "april", "may", "ju
 var daysOfTheWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var birthYear = prompt("Enter the year you were born: ")
 
 
