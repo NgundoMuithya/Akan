3c1a9d49c02e5e0d5d194c8b1c2f4ad0a120fd1c
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-20 17:04:43 +0300

    Create a list of female Akan names.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,6 @@
 var months = ["placeholder", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 var daysOfTheWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-
+var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
 
