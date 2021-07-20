e4335f8c4dff10f49369e6998d1c8fa09bb0b88f
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-20 16:58:49 +0300

    Create a list of days of the week.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,3 @@
 var months = ["placeholder", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+var daysOfTheWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 
