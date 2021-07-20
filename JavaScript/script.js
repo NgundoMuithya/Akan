23083fe7e9437850ce688ab9940ca47f1b2958fa
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-20 18:13:34 +0300

    Use control flow to show invalid entry of birth day.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,26 @@
 var months = ["placeholder", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var birthYear = prompt("Enter the year you were born in: ")
+var birthYear = prompt("Enter the year you were born in e.g. '2021': ")
 var century = parseInt(birthYear.slice(0, 2))
 var year = parseInt(birthYear.slice(2, 4))
-var birthMonth = prompt("Enter the month you were born in: ")
+var birthMonth = prompt("Enter the name of the month you were born in e.g. 'January': ")
 var month = months.indexOf(birthMonth.toLowerCase())
 var birthDay = prompt("On which day of that month were you born e.g. If born on the 14th enter '14': ")
 var day = parseInt(birthDay)
-var gender = confirm("Are you male? (Hit OK if 'yes' and Cancel if 'no'): ")
-var indexofAkanName = (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7
-if (gender === true) {
+if (day <= 0 || day >= 32) {
+    alert("Invalid day of month entered! The day of the month cannot be 0 or less than 0 and cannot be 32 or greater than 32.")
+}
+else {
+    var gender = confirm("Are you male? (Hit OK if 'yes' and Cancel if 'no'): ")
+    var indexofAkanName = (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7
+    if (gender === true) {
     var akanName = maleAkanNames[indexofAkanName]
     alert("Your Akan name is " + akanName + ".")
-}
-if (gender === false) {
+    }
+    else {
     var akanName = femaleAkanNames[indexofAkanName]
     alert("Your Akan name is " + akanName + ".")
+    }
 }
+
