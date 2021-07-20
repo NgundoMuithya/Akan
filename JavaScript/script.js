4ccbf25711893dbe4de7535b6ae04783be22f476
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-20 17:45:48 +0300

    Confirm the gender.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,4 @@
 var months = ["placeholder", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-var daysOfTheWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var birthYear = prompt("Enter the year you were born in: ")
@@ -9,4 +8,5 @@ var birthMonth = prompt("Enter the month you were born in: ")
 var month = months.indexOf(birthMonth.toLowerCase())
 var birthDay = prompt("On which day of that month were you born e.g. If born on the 14th enter '14': ")
 var day = parseInt(birthDay)
+var gender = confirm("Are you male? (Hit OK if 'yes' and Cancel if 'no'): ")
 
