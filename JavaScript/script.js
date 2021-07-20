ac6bccd175703449ff0788dab7b39fa67df67a62
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-20 17:48:44 +0300

    Derive the Akan Name Index.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,4 +9,4 @@ var month = months.indexOf(birthMonth.toLowerCase())
 var birthDay = prompt("On which day of that month were you born e.g. If born on the 14th enter '14': ")
 var day = parseInt(birthDay)
 var gender = confirm("Are you male? (Hit OK if 'yes' and Cancel if 'no'): ")
-
+var indexofAkanName = (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day)
